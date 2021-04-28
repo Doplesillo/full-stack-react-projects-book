a19eec2018f897a93f667bad36d9e1790359f00a
Manuel Chairez-Macias <doplesillo@gmail.com>
2021-04-27 22:01:59 -0500

    Chapter 04 Integrating user auth and protected routes

--- skeleton-backend/server/express.js
+++ skeleton-backend/server/express.js
@@ -6,6 +6,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template'
 import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 const app = express()
 // Body-parser deprecated since express 4.16
@@ -17,6 +18,15 @@ app.use(compress())
 app.use(helmet())
 app.use(cors())
 
-app.use('/', userRoutes)
+app.use('/', authRoutes)
 
+
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ "error" : err.name + ": " + err.message })
+    } else if (err) {
+        res.status(400).json({ "error" : err.name + ": " + err.message })
+        console.log(err)
+    }
+})
 export default app
